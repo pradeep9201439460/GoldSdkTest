ee96f7285ea513c52f3bbe08bbacfd773c0a1372
Siddhant Baviskar <siddhant.baviskar@safegold.in>
2020-01-10 12:54:32 +0530

    AES encrypted request and response

--- app/src/main/java/com/example/goldsdktest/MainActivity.java
+++ app/src/main/java/com/example/goldsdktest/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.goldsdktest;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.safegold.InitializationException;
 import com.safegold.SafeGoldModule;
 import com.safegold.interfaces.BackPressedInterface;
 import com.safegold.interfaces.HeartBeatInterface;
